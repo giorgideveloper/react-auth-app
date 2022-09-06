eca88c640819bf34fd8e4cd4533193b67e851b30
giorgideveloper <gorgiladze888@gmail.com>
2022-09-06 17:13:42 +0400

    add tailwind css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<h1>Twitter 🕊</h1>
-			</header>
+		<div className='flex flex-row justify-center text-center align-middle '>
+			<div className='basis-1/4 bg-sky-500/75 '>01</div>
+			<div className='basis-1/4 bg-sky-500/75 '>02</div>
+			<div className='basis-1/2 bg-sky-500/75 '>03</div>
 		</div>
 	);
 }
